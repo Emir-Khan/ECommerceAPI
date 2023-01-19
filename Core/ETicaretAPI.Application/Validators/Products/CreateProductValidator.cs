92d3c56b1dbb605765adcfbaf9b721c20c91f62c
Emir-Khan <emirkhan5534@gmail.com>
2023-01-19 15:08:43 +0300

    Fluent Validation

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,28 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("Please fill the Name field")
+                .MinimumLength(3).MaximumLength(150).WithMessage("Please enter the Name field between 5 and 150 characters");
+
+            RuleFor(p => p.Stock)
+                .NotEmpty().WithMessage("Please fill the Stock field")
+                .Must(s => s >= 0).WithMessage("Stock field cannot be negative number");
+
+            RuleFor(p => p.Price)
+                .NotEmpty().WithMessage("Please fill the Price field")
+                .Must(p => p >= 0).WithMessage("Price field cannot be negative number");
+        }
+    }
+}
--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,4 +1,8 @@
+using ETicaretAPI.Application.Validators.Products;
+using ETicaretAPI.Infrastructure.Filters;
 using ETicaretAPI.Persistence;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +12,8 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
     policy.WithOrigins("https://localhost:4200", "http://localhost:4200").AllowAnyHeader().AllowAnyMethod()
 ));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>()).ConfigureApiBehaviorOptions(options=> options.SuppressModelStateInvalidFilter= true);
+builder.Services.AddFluentValidationAutoValidation().AddValidatorsFromAssemblyContaining<CreateProductValidator>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
