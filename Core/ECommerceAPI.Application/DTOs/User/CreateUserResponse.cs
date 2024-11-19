78fad3f47ba7030e1cb1226c9eb0047bf29d593b
Emirhan Köseoğlu <emirkhan5534@gmail.com>
2024-11-20 01:36:31 +0300

    Usings Cleared & Formatted

--- Core/ECommerceAPI.Application/DTOs/User/CreateUserResponse.cs
+++ Core/ECommerceAPI.Application/DTOs/User/CreateUserResponse.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ECommerceAPI.Application.DTOs.User
+﻿namespace ECommerceAPI.Application.DTOs.User
 {
     public class CreateUserResponse
     {
+        public string UserId { get; set; }
         public bool Succeeded { get; set; }
         public string Message { get; set; }
     }
--- Core/ECommerceAPI.Application/Features/Commands/Product/CreateProduct/CreateProductCommandResponse.cs
+++ Core/ECommerceAPI.Application/Features/Commands/Product/CreateProduct/CreateProductCommandResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ECommerceAPI.Application.Features.Commands.Product.CreateProduct
+﻿namespace ECommerceAPI.Application.Features.Commands.Product.CreateProduct
 {
     public class CreateProductCommandResponse
     {
